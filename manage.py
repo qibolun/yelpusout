01bd5a49a7b303ce727a939da024baeed6a4d85c
Bolun Qi <bolunqi2@illinois.edu>
2017-07-27 12:05:03 -0700

    docker for flask

---
+++ manage.py
@@ -0,0 +1,32 @@
+import unittest
+
+from flask_script import Manager
+
+from project import create_app, db
+from project.api.models import User
+
+
+app = create_app()
+manager = Manager(app)
+
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+if __name__ == '__main__':
+    manager.run()
---
+++ project/tests/test_users.py
@@ -0,0 +1,80 @@
+import json
+
+from project.tests.base import BaseTestCase
+
+
+class TestUserService(BaseTestCase):
+    """Tests for the Users Service."""
+
+    def test_users(self):
+        """Ensure the /ping route behaves correctly."""
+        response = self.client.get('/ping')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('pong!', data['message'])
+        self.assertIn('success', data['status'])
+
+    def test_add_user(self):
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='michael',
+                    email='michael@realpython.com'
+                )),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('michael@realpython.com was added!', data['message'])
+            self.assertIn('success', data['status'])
+    def test_add_user_invalid_json(self):
+        """Ensure error is thrown if the JSON object is empty."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict()),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_invalid_json_keys(self):
+        """Ensure error is thrown if the JSON object does not have a username key."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(email='michael@realpython.com')),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_duplicate_user(self):
+        """Ensure error is thrown if the email already exists."""
+        with self.client:
+            self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='michael',
+                    email='michael@realpython.com'
+                )),
+                content_type='application/json',
+            )
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='michael',
+                    email='michael@realpython.com'
+                )),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn(
+                'Sorry. That email already exists.', data['message'])
+            self.assertIn('fail', data['status'])
