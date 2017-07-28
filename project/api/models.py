16e0b689cab364cc7ce1f065055e1e80b22bcb07
andy1n9 <kangying@yelp.com>
2017-07-27 17:37:55 -0700

    finish api for group creation & joining

--- project/api/models.py
+++ project/api/models.py
@@ -15,3 +15,24 @@ class User(db.Model):
         self.username = username
         self.email = email
         self.created_at = datetime.datetime.utcnow()
+
+class Group(db.Model):
+    __tablename__ = "group"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    groupname = db.Column(db.String(128), nullable=False)
+    location_latitude = db.Column(db.Float, nullable=True)
+    location_longitude = db.Column(db.Float, nullable=True)
+    radius = db.Column(db.Integer, nullable=True)
+    price = db.Column(db.String(128), nullable=True)
+    openat = db.Column(db.Integer, nullable=True)
+    categories = db.Column(db.String(128), nullable=True)
+    member_number = db.Column(db.Integer, default=0)
+
+    def __init__(self, groupname, location, radius, price, openat, categories):
+        self.groupname = groupname
+        self.location_latitude = location['latitude']
+        self.location_longitude = location['longitude']
+        self.radius = radius
+        self.price = price
+        self.openat = openat
+        self.categories = categories
--- project/api/views.py
+++ project/api/views.py
@@ -6,6 +6,7 @@ from project import db
 
 
 users_blueprint = Blueprint('users', __name__, template_folder='./templates')
+group_blueprint = Blueprint('group')
 
 @users_blueprint.route('/', methods=['GET'])
 def index():
@@ -43,6 +44,73 @@ def add_user():
             response_object = {
                 'status': 'fail',
                 'message': 'Sorry. That email already exists.'
+            }
+            return jsonify(response_object), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
+
+@group_blueprint.route('/group', methods=['POST'])
+def create_group():
+    post_data = request.get_json()
+    if not post_data:
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
+    group_name = post_data.get('groupname')
+    location = post_data.get('location')
+    radius = post_data.get('radius')
+    price = post_data.get('price')
+    openat = post_data.get('openat')
+    categories = post_data.get('categories')
+    try:
+        newgroup = Group(groupname=group_name, location=location, radius=radius, price=price, openat=openat, categories=categories)
+        db.session.add(newgroup)
+        db.session.commit()
+        response_object = {
+            'status': 'success',
+            'message': f'{group_name} was created!',
+            'url': newgroup.id,
+        }
+        return jsonify(response_object), 201
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
+
+@group_blueprint.route('/group/<groupid>', methods=['GET'])
+def join_group(groupid):
+    if not groupid:
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
+    try:
+        group = Group.query.filter_by(id=groupid).first()
+        if group:
+            group.member_number += 1
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': f'{groupid} was found!',
+                'groupname': group.groupname,
+                'userid': group.member_number,
+            }
+            return jsonify(response_object), 200
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Sorry. The group doesn\'t exist.'
             }
             return jsonify(response_object), 400
     except exc.IntegrityError as e:
